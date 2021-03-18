b01c81868bc5a6bdd3377c95c9e946679b24635a
eyal <eyal444555@gmail.com>
2021-03-19 01:09:13 +0200

    adding current score

--- src/App.js
+++ src/App.js
@@ -11,9 +11,38 @@ class App extends  React.Component{
     player2Score:0,
     currentScore1:0,
     currentScore2:0,
-    turn:1
+    turn:1,
+    image1:'',
+    image2:''
 
   } 
+
+
+  getCurrentScore = (score) =>{
+
+    if(this.state.turn === 1){
+      this.setState({currentScore1:this.state.currentScore1 + score})
+    }else{
+      this.setState({currentScore2:this.state.currentScore2 + score})
+    } 
+
+  }
+
+  RollDices = (e) =>{
+
+    let cube1 = Math.floor(Math.random()*6+1)
+    let cube2 = Math.floor(Math.random()*6+1)
+
+    this.setState({
+      image1:`dice${cube1}`,
+      image2:`dice${cube2}`
+    })
+
+
+   this.getCurrentScore(cube1+cube2)
+
+
+  }
 
   
 render(){
@@ -23,8 +52,9 @@ render(){
           <Player playerNumber ="2" turn = {this.state.turn} currentScore = {this.state.currentScore2} Score = {this.state.player2Score}></Player>
           <div className="boardSettings">
               <button>New game</button>
-              <img src="" alt="asfassfa"/>
-              <img src="" alt="sadfdsasf"/>
+              <img src={`/images/`+this.state.image1+'.png'} alt=""/>
+              <img src={`/images/`+this.state.image2+'.png'} alt=""/>
+              <button onClick={this.RollDices}>Roll Dice</button>
               <button>Hold</button>
           </div>
       </div>
