8bf8bac3f5ecc5bfb9be369865229800c51f2bce
eyal <eyal444555@gmail.com>
2021-03-18 20:13:42 +0200

    adding first style

--- src/App.js
+++ src/App.js
@@ -19,8 +19,14 @@ class App extends  React.Component{
 render(){
     return (
       <div className="App">
-          <Player playerNumber ="1" turn = {this.state.turn}></Player>
-          <Player playerNumber ="2" turn = {this.state.turn}></Player>
+          <Player playerNumber ="1" turn = {this.state.turn} currentScore = {this.state.currentScore1} Score = {this.state.player1Score}></Player>
+          <Player playerNumber ="2" turn = {this.state.turn} currentScore = {this.state.currentScore2} Score = {this.state.player2Score}></Player>
+          <div className="boardSettings">
+              <button>New game</button>
+              <img src="" alt="asfassfa"/>
+              <img src="" alt="sadfdsasf"/>
+              <button>Hold</button>
+          </div>
       </div>
     );
 
--- src/Components/player.js
+++ src/Components/player.js
@@ -9,18 +9,30 @@ export default class Components extends Component {
     render() {
 
         
-        let Active = ''
+        let Active;
 
         if(this.props.turn == this.props.playerNumber){
             Active = 'active'
         }else{
-            Active = ''
+            Active = 'unactive'
         }
 
         return (
             <div className ={"player " + Active}>
+                <div>
                 <h1>Player{this.props.playerNumber}</h1>
-
+                <h1>{this.props.Score}</h1>
+                </div>
+                <div>
+                    <div className="testing">
+                    <h1>
+                        currentScore
+                    </h1>
+                    <h1>
+                        {this.props.currentScore}
+                    </h1>
+                    </div>
+                </div>
             </div>
         )
     }
