0fc2fa41554e277d3b17a788eb01be5184feb994
eyal <eyal444555@gmail.com>
2021-03-18 18:33:05 +0200

    first files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './App.css';
+
+
+class App extends  React.Component{
+
+
+  state ={
+
+    player1Score:0,
+    player2Score:0,
+    currentScore:0
+
+
+  } 
+
+  
+render(){
+    return (
+      <div className="App">
+  
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
